bce1fbbfad7b0fecbcb61062fb50a93745f04a6b
MrAhmedElsayed <ahmedsoftwaredevelopment@gmail.com>
2022-10-07 01:30:17 +0300

    update with vuetify

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# AutoCAD-to-Excel
-Using AutoCAD JavaScript APIs, I created a plugin to export numbered points to Excel and also create a table for them, supports completion also when interrupted for any reason, and is also easy to install.
+# autocad-javascript-api
 
-## please Note:  [AutoDesk Docs](https://help.autodesk.com/view/OARX/2023/ENU/?guid=adsk_jsdev_autocad_javascript_api_about)
+## Project setup
+```
+yarn install
+```
 
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
 
-## docs links
-- [AutoCAD-JavaScript-and-Cloud-2015#handout](https://www.autodesk.com/autodesk-university/class/AutoCAD-JavaScript-and-Cloud-2015#handout)
+### Compiles and minifies for production
+```
+yarn build
+```
 
-- [AutoCAD-JavaScript-and-Cloud-2015#presentation](https://www.autodesk.com/autodesk-university/class/AutoCAD-JavaScript-and-Cloud-2015#presentation)
+### Lints and fixes files
+```
+yarn lint
+```
 
-
-
-
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,70 +1,12 @@
-// https://v2.vuejs.org/v2/guide/
-var app = new Vue({
-  el: "#app",
-  data: {
-    // output required info
-    outputDirectory: "C:\\Users\\ahmed\\Desktop",
-    fileName: "points",
-    fileFormate: "csv", // file format
-    showReloadToadt: false,
-    selectedCoordinates: [
-      {
-        pn: 1,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-      {
-        pn: 2,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-      {
-        pn: 3,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-      {
-        pn: 4,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-      {
-        pn: 5,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-      {
-        pn: 6,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-      {
-        pn: 7,
-        x: 776160.013,
-        y: 2233695.263,
-        z: 1604.234,
-      },
-    ],
-  },
-  computed: {
-    outputFile() {
-      return (
-        this.outputDirectory + "\\" + this.fileName + "." + this.fileFormate
-      );
-    },
-    pointsCount() {
-      return this.selectedCoordinates.length;
-    },
-  },
-  methods: {
-    reverseMessage: function () { // just placeholder for methods
-      this.fileName = this.fileName.split("").reverse().join("");
-    },
-  },
-});
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
